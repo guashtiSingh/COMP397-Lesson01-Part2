ee9bc138a59917da5403a3f09b5ed9721496f168
guashtiSingh <ashtisingh@hotmail.com>
2015-05-05 09:53:57 -0400

    added Hello World Content

---
+++ COMP-Lesson01-Part02/Scripts/game.ts
@@ -0,0 +1,18 @@
+﻿class Person {
+    //Constructor +++++++++++++++++++++++++++++++++++++
+    constructor() {
+    }
+
+    //Public Metods +++++++++++++++++++++++++++++++++++
+    public saysHello() {
+        console.log("Hello World!");
+    }
+
+}
+
+
+function init() {
+    var myPerson = new Person();
+    myPerson.saysHello();
+}
+
